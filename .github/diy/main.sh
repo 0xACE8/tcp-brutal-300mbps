27129955816027b73fa3100338f3660417346800
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2025-12-07 19:44:28 -0800

    Create main.sh

---
+++ .github/diy/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+function git_clone() {
+  git clone --depth 1 $1 $2 || true
+ }
+function git_sparse_clone() {
+  branch="$1" rurl="$2" localdir="$3" && shift 3
+  git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+  cd $localdir
+  git sparse-checkout init --cone
+  git sparse-checkout set $@
+  mv -n $@ ../
+  cd ..
+  rm -rf $localdir
+  }
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+
+## kernel Tcp-Brutal
+git clone --depth 1 https://github.com/apernet/tcp-brutal tb && mv -n tb/* ./; rm -rf tb
+
+
+exit 0
